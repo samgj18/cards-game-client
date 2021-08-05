15f60ce68299be2846f72198c4b165180be6599b
samgj18 <samgj_18@hotmai.com>
2021-08-04 22:53:43 -0500

    Initial Commit

---
+++ cards/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  cards
+//
+//  Created by Samuel Gómez Jiménez on 4/08/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ cards/cardsApp.swift
@@ -0,0 +1,17 @@
+//
+//  cardsApp.swift
+//  cards
+//
+//  Created by Samuel Gómez Jiménez on 4/08/21.
+//
+
+import SwiftUI
+
+@main
+struct cardsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
